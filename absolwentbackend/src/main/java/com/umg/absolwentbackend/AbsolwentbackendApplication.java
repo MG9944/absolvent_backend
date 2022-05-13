2df25e5d8248b553f4bb2b158333841baaef37e8
MG9944 <matthiasgdula@gmail.com>
2022-05-13 09:30:59 +0200

    Creating Spring Boot Project

---
+++ absolwentbackend/src/main/java/com/umg/absolwentbackend/AbsolwentbackendApplication.java
@@ -0,0 +1,13 @@
+package com.umg.absolwentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsolwentbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbsolwentbackendApplication.class, args);
+    }
+
+}
