d51121e72c99e1e58533ee0eeb4d595ef4b6f0ff
Torak2506 <torak2506@gmail.com>
2022-05-21 20:00:30 +0200

    University model corrected

--- absolwentbackend/src/main/java/com/umg/absolwentbackend/models/University.java
+++ absolwentbackend/src/main/java/com/umg/absolwentbackend/models/University.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.umg.absolwentbackend.models;
 
 
@@ -20,30 +19,4 @@ public class University {
         return login;
     }
 }
-=======
-package com.umg.absolwentbackend.models;
-
-
-public class University {
-    private String password;
-    private Date dateOfLastQuestionnaire;
-    private String questionnaireFrequency;
-    private String login;
-
-
-    public University(String password,String login) {
-        this.password = password;
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-
-}
->>>>>>> 99b24c53340dc4a62e0c191c18dd46d052843e37
+    
